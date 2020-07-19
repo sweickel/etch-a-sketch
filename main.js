0f29564394fe93d7a05905da3b6049329e5a8b35
Sean Weickel <sweickel@gmail.com>
2020-07-19 16:57:16 -0400

    finished project functionality

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 function createGrid() {
 	const wrapper = document.querySelector('.wrapper');
 	const wrapperLength = wrapper.children.length;
-	//remove divs first, then create
+	
+	//clears grid
 	for (let j = wrapperLength - 1; j >= 0; j--) {
 		wrapper.removeChild(wrapper.children[j]);
 	}
 
-	for (let i = 0; i < 16; i++) {
+	//creates new grid
+	let result = document.querySelector('.cells').value;
+	let allCells = result * result;
+	for (let i = 0; i < allCells; i++) {
 		const cell = document.createElement('div');
 		cell.classList.add('cell');	
 		wrapper.appendChild(cell);
-		cell.addEventListener('click', (event) => {
+		cell.addEventListener('mouseover', (event) => {
 			cell.className = "cell_fill";
 		});
 	}
+		
+	wrapper.style.cssText = `grid-template-columns: repeat(${result}, 1fr); grid-template-rows: repeat(${result}, 1fr);`;
 }
 
 function listeners() {
@@ -25,5 +31,6 @@ function listeners() {
 
 window.addEventListener('load', (event) => {
 	listeners();
+	document.querySelector('.cells').value = 4;
 	
 });
